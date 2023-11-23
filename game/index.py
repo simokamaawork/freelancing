7aff79a5a87d6a12f9be6fd79871c442ba1b548f
Simon Kamau <simonkamau690@gmail.com>
2023-11-23 15:13:27 +0300

    index.py

---
+++ game/index.py
@@ -0,0 +1,80 @@
+import time
+
+# Define variables
+player_name = ""
+inventory = []
+stage = 1
+
+# Introduction and game objective
+def game_intro():
+    print("Welcome to the Text Adventure Game!")
+    time.sleep(1)
+    print("You find yourself in a mysterious world...")
+    time.sleep(1)
+    print("Your objective is to reach the end of the adventure.")
+    time.sleep(1)
+    print("Be cautious with your choices!")
+    time.sleep(1)
+    print("=" * 40)
+
+# Stage 1
+def stage_one():
+    global stage
+    print("Stage 1: The Forest")
+    time.sleep(1)
+    choice = input("Do you want to go left or right? ").lower()
+    if choice == "left":
+        print("You discover a hidden path.")
+        inventory.append("Map")
+        stage += 1
+    elif choice == "right":
+        print("You encounter a dangerous creature.")
+        fail()
+    else:
+        print("Invalid choice. Try again.")
+        stage_one()
+
+# Stage 2
+def stage_two():
+    global stage
+    print("Stage 2: The Cave")
+    time.sleep(1)
+    print("You use the Map to navigate through the cave.")
+    time.sleep(1)
+    choice = input("Continue forward or go back? ").lower()
+    if choice == "forward":
+        print("You find a key and unlock a door.")
+        inventory.append("Key")
+        stage += 1
+    elif choice == "back":
+        print("You encounter a collapsing tunnel.")
+        fail()
+    else:
+        print("Invalid choice. Try again.")
+        stage_two()
+
+# Win function
+def win():
+    print("Congratulations! You have successfully completed the Text Adventure Game.")
+
+# Fail function
+def fail():
+    print("Game over! Better luck next time.")
+    exit()
+
+# Main game loop
+def play_game():
+    game_intro()
+
+    while stage <= 5:
+        if stage == 1:
+            stage_one()
+        elif stage == 2:
+            stage_two()
+        # Add more stages as needed
+
+    # If the player reaches this point, they have won the game
+    win()
+
+# Start the game
+play_game()
