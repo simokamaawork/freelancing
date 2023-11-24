313952fbd271f7b0b15a07c9588d31839920a118
Simon Kamau <simonkamau690@gmail.com>
2023-11-24 18:19:24 +0300

    mb
    mb

---
+++ mobilecomputing/index.js
@@ -0,0 +1,17 @@
+// alert("hello lucy");
+// javascript
+// data types--------------------primitives     /   Reference
+
+javascript
+node 
+expressjs
+reactjs 
+nextjs
+redux 
+graphql
+prisma 
+tailwind
+radix 
+reactnative 
+expo 
+
